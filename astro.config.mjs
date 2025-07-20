b8bb2f6c7b6fb280d291c619029524da698604de
Gianluca Cornetta <gianluca.cornetta@gmail.com>
2025-07-20 14:33:36 +0200

    Initial project scaffold

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import alpinejs from '@astrojs/alpinejs';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [alpinejs()],
+  adapter: cloudflare()
+});
