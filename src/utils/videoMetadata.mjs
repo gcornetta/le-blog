44f82b229a837ca1a1b3ccfd9883c5ee7067994f
Gianluca Cornetta <gianluca.cornetta@gmail.com>
2025-07-27 12:08:24 +0200

    improved video d escription text styling

--- src/utils/videoMetadata.mjs
+++ src/utils/videoMetadata.mjs
@@ -1,7 +1,7 @@
 export function getFeaturedVideo() {
   return {
     title: "Understanding GFET Design",
-    description: "An in-depth walkthrough of graphene FETs, from theory to layout and simulation.",
+    description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
     youtubeId: "dQw4w9WgXcQ" // Replace with your actual featured video ID
   };
 }
